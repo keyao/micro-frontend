47d191c160ebbd97ee7920b2e2f64867cf7ca3dd
keyao@motern.com <keyao1996712>
2022-04-23 22:50:37 +0800

    升级

---
+++ main/src/layout/AppMain.vue
@@ -0,0 +1,18 @@
+<template>
+    <div class="app-main">
+        <div>
+            <router-view></router-view>
+            <div id="container"></div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "AppMain"
+    }
+</script>
+
+<style scoped>
+
+</style>
--- main/src/layout/Index.vue
+++ main/src/layout/Index.vue
@@ -1,16 +1,39 @@
 <template>
     <div class="layout">
         <div class="layout-top"></div>
-        <div>
-            <div class="nav"></div>
+        <div class="layout-main">
+            <div class="layout-main-nav">
+                <el-menu>
+                    <el-menu
+                            active-text-color="#ffd04b"
+                            background-color="#545c64"
+                            class="el-menu-vertical-demo"
+                            default-active="2"
+                            text-color="#fff">
+                        <el-menu-item index="2">
+                            <el-icon><icon-menu /></el-icon>
+                            <span>vue</span>
+                        </el-menu-item>
+                        <el-menu-item route="/index" index="3">
+                            <el-icon><document /></el-icon>
+                            <span>react</span>
+                        </el-menu-item>
+                        <el-menu-item route="/home" index="4">
+                            <el-icon><setting /></el-icon>
+                            <span>home</span>
+                        </el-menu-item>
+                    </el-menu>
+                </el-menu>
+            </div>
             <div class="content">
-                <router-view></router-view>
+                <AppMain></AppMain>
             </div>
         </div>
     </div>
 </template>
 
 <script lang="ts" setup>
+    import AppMain from "./AppMain.vue";
     const state = reactive({
         name:'111'
     })
@@ -23,6 +46,15 @@
         &-top{
             height:60px;
             background: #2c3e50;
+        }
+        &-main{
+            display: flex;
+            &-nav{
+                width: 210px;
+            }
+            &-content{
+                width: calc(100% - 210px);
+            }
         }
     }
 </style>
